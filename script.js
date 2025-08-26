87fa377d29d0d2098a8778643c5a3f7e90eb94e5
sakshii262005 <mahajanyogita03@gmail.com>
2025-08-26 19:46:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-window.addEventListener("scroll",function(){
-  const navbar=document.getElementById("navbar");
-
-  if(window.scrolly > 50){
-    navbar.classList.add("scrolled");
-  }else{
-    navbar.classList.remove("scrolled");
-  }
-
+console.log("Portfolio Website Loaded Successfully!");
+document.addEventListener("DOMContentLoaded", () => {
+  alert("Welcome to my Portfolio Website 🚀");
 });
