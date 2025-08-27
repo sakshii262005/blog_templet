b6b39be1ee4221b4c06c66e731bf485bb779233a
sakshii262005 <mahajanyogita03@gmail.com>
2025-08-27 16:35:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,90 @@
-let seconds = 00;
-let tens = 00;
-let mins = 00;
-let getSeconds = document.querySelector('.seconds');
-let getTens = document.querySelector('.tens');
-let getMins = document.querySelector('.mins');
-let btnStart = document.querySelector('.btn-start');
-let btnStop = document.querySelector('.btn-stop');
-let btnReset = document.querySelector('.btn-reset');
-let interval;
+const statusDisplay = document.querySelector('.game--status');
 
-btnStart.addEventListener('click', () => {
-    clearInterval(interval);
-    inverval = setInterval(startTimer, 10);
-})
-btnStop.addEventListener('click', () => {
-    clearInterval(inverval);
-})
-btnReset.addEventListener('click', () => {
-    clearInterval(inverval);
-    tens = '00';
-    seconds = '00';
-    mins = '00';
-    getSeconds.innerHTML = seconds;
-    getTens.innerHTML = tens;
-    getMins.innerHTML = mins;
-})
+let gameActive = true;
 
-function startTimer(){
-    tens++;
-    if(tens <= 9){
-        getTens.innerHTML = '0' + tens;
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => "winner!";
+const drawMessage = () => "It's a draw!";
+const currentPlayerTurn = () => "It's your turn";
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+function handleCellClick(clickedCellEvent) {   
+        const clickedCell = clickedCellEvent.target;
+        const clickedCellIndex = parseInt(
+          clickedCell.getAttribute('data-cell-index')
+        );
+    
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
+        }
+   
+        handleCellPlayed(clickedCell, clickedCellIndex);
+        handleResultValidation();
+}
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    
+        gameState[clickedCellIndex] = currentPlayer;
+        clickedCell.innerHTML = currentPlayer;
     }
-    if(tens > 9){
-        getTens.innerHTML = tens;
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            let a = gameState[winCondition[0]];
+            let b = gameState[winCondition[1]];
+            let c = gameState[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break
+            }
+        }
+ if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
     }
-    if(tens > 99){
-        seconds++;
-        getSeconds.innerHTML = '0' + seconds;
-        tens = 0;
-        getTens.innerHTML = '0' + 0;
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
     }
-    if(seconds > 9){
-        getSeconds.innerHTML = seconds;
-    }
-    if(seconds > 59){
-        mins++;
-        getMins.innerHTML = '0' + mins;
-        seconds = 0;
-        getSeconds.innerHTML = '0' + 0;
-    }
-    if(mins > 9){
-        getSeconds.innerHTML = mins;
-    }
+
+    handlePlayerChange();
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell')
+               .forEach(cell => cell.innerHTML = "");
 }
